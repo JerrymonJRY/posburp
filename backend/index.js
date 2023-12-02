84e3ce64283ab85904844ac7fc0659be1a58f483
JerrymonJRY <58291290+JerrymonJRY@users.noreply.github.com>
2023-12-02 11:44:17 +0400

    pox

--- backend/index.js
+++ backend/index.js
@@ -28,15 +28,15 @@ const PORT = process.env.PORT || 4000;
 dbConnect();
 
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://starlit-meerkat-1f8155.netlify.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'https://starlit-meerkat-1f8155.netlify.app');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+// });
 
- app.use(cors({ origin: 'https://starlit-meerkat-1f8155.netlify.app' }));
-//app.use(cors());
+//  app.use(cors({ origin: 'https://starlit-meerkat-1f8155.netlify.app' }));
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
